247e0df7d9442fa05a9a8769a79255868a2e62ec
sarasabzali <66485122+sarasabzali@users.noreply.github.com>
2020-06-27 21:57:25 +0300

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("welcome to my web site ...Don't forget to sign in.")
+
+let neme = 'sara';
+let age = 17;
+let person ={
+name: 'sara',
+age: 17
+
+};
+
+person.name ='sarah';
+person['name'] = 'moho';
+console.log(person.name);
